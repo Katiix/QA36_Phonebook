667f41cffdd89e5fafc96324cfa0375b085c3c82
Katiix <katuha1995@gmail.com>
2023-01-10 21:07:16 +0200

    registration tests

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,25 +1,54 @@
 package manager;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class HelperUser extends HelperBase{
+import java.util.List;
+
+public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
         super(wd);
     }
 
-    public void openLoginRegistrationForm(){
+    public void openLoginRegistrationForm() {
         wd.findElement(By.cssSelector("a[href='/login']")).click();
     }
-    public void fillLoginRegistrationForm(String email, String password){
-        type(By.cssSelector("input[name='email']"),email);
-        type(By.cssSelector("input[name='password']"),password);
+
+    public void fillLoginRegistrationForm(String email, String password) {
+        type(By.cssSelector("input[name='email']"), email);
+        type(By.cssSelector("input[name='password']"), password);
     }
 
-    public void submitLogin(){
+    public void submitLogin() {
         click(By.cssSelector("button[name='login']"));
     }
 
 
+    public boolean isLogged() {
+        //return wd.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
+        List<WebElement> list = wd.findElements(By.xpath("//button[text()='Sign Out']"));
+        return list.size() > 0;
+    }
 
+    public void logout() {
+        click(By.xpath("//button[text()='Sign Out']"));
+    }
+
+    public boolean isErrorMessageDisplayed(String message) {
+        Alert alert = wd.switchTo().alert();
+        String text = alert.getText();
+        System.out.println(text);
+        //click ok
+        alert.accept();
+        //click cancel
+        //alert.dismiss()
+        //alert.sendKeys("Hello");
+        return text.contains(message);
+    }
+
+    public void submitRegistration() {
+        click(By.cssSelector("[name='registration']"));
+    }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,18 +1,56 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
+
+    @BeforeMethod
+    public void preCondition(){
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
     @Test
     public void loginSuccess(){
         app.getHelperUser().openLoginRegistrationForm();
         app.getHelperUser().fillLoginRegistrationForm("kate@gmail.com","Kkate12345$");
         app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isLogged());
+        //logout
+
+    }
+
+    public void loginSuccessNew(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("kate@gmail.com","Kkate12345$");
+        app.getHelperUser().submitLogin();
+        //logout
+
     }
     @Test
-    public void loginWrongEmail(){}
+    public void loginWrongEmail(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("kateagmail.com","Kkate12345$");
+        app.getHelperUser().submitLogin();
+        Assert.assertFalse(app.getHelperUser().isLogged());
+        Assert.assertTrue(app.getHelperUser().isErrorMessageDisplayed("Wrong email or password"));
+    }
     @Test
-    public void loginWrongPassword(){}
+    public void loginWrongPassword(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("kate@gmail.com","ka123");
+        app.getHelperUser().submitLogin();
+        Assert.assertFalse(app.getHelperUser().isLogged());
+        Assert.assertTrue(app.getHelperUser().isErrorMessageDisplayed("Wrong email or password"));
+    }
     @Test
-    public void loginUnregisteredUser(){}
+    public void loginUnregisteredUser(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("sfkdlfgdl@.com","Kkate12345$");
+        app.getHelperUser().submitLogin();
+        Assert.assertFalse(app.getHelperUser().isLogged());
+        Assert.assertTrue(app.getHelperUser().isErrorMessageDisplayed("Wrong email or password"));
+    }
 }
